2e477b3f689eea722a544000da7d8311971fcf4e
violog <51th.apprent1ce.f0rce@gmail.com>
2024-05-17 16:50:43 +0300

    Add key generation and keys in script

--- register_script_test.go
+++ register_script_test.go
@@ -2,30 +2,96 @@ package identity_test
 
 import (
 	"bytes"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"math/big"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/iden3/go-iden3-crypto/babyjub"
 	"github.com/rarimo/registration-relayer/resources"
 	"github.com/rarimovoting/identity"
 )
 
-func TestCreateAndRegister(t *testing.T) {
-	// TODO impl and set, get from env
-	var provider identity.StateProvider
-	var relayerURL string
-	var baseArgs = registerArgs{}
+const keysCount = 3
 
+func TestGenerateCrypto(t *testing.T) {
+	for j := 0; j < keysCount; j++ {
+		key := babyjub.NewRandPrivKey()
+
+		i, e := big.NewInt(2), big.NewInt(248)
+		maxKeySize := i.Exp(i, e, nil)
+
+		secret, err := rand.Int(rand.Reader, maxKeySize)
+		if err != nil {
+			t.Fatal(fmt.Errorf("error generating secret: %v", err))
+		}
+
+		nullifier, err := rand.Int(rand.Reader, maxKeySize)
+		if err != nil {
+			t.Fatal(fmt.Errorf("error generating nullifier: %v", err))
+		}
+
+		var comma string
+		if j < keysCount-1 {
+			comma = ","
+		}
+
+		fmt.Printf(`
+		{
+			identityArgs: identityArgs{
+				secretKeyHex: "0x%s",
+				secretHex:    "0x%s",
+				nullifierHex: "0x%s",
+			},
+		}%s
+`, hex.EncodeToString(key[:]), secret.Text(16), nullifier.Text(16), comma)
+	}
+}
+
+func TestCreateAndRegister(t *testing.T) {
+	// TODO impl
+	var provider identity.StateProvider
+
+	var env = fromEnv()
+
+	// generate in TestGenerateCrypto and paste here
 	testCases := []struct {
 		identityArgs
 		registerArgs
 	}{
 		{
-			identityArgs{provider: provider},
-			baseArgs,
+			identityArgs: identityArgs{
+				secretKeyHex: "0xb34c4f6ef68d95d72b49c7bec5b8ecba55d2e0a82b81b55d8e6dd292107ea51d",
+				secretHex:    "0x264cc18cef255b00af5188b5ecd96c88e10fff05f27a3b116f4cdca9f02439",
+				nullifierHex: "0x94ce952a70c516d4415869e6ea76ddc9a87ca9de412dcc0d3ce5975ed14303",
+			},
 		},
+
+		{
+			identityArgs: identityArgs{
+				secretKeyHex: "0xf361688e574840b2d1b1bf4af15b548e048fccba581eec95b9b4bda6844a85c1",
+				secretHex:    "0x1efe93dce05281b56be7653ea585dc8cba94eec80286da4e7458542fc2d224",
+				nullifierHex: "0xf1ed6b705caf03e7cad9c488e5f3517c2b0a7c59426a440e467d5d707cb6f6",
+			},
+		},
+
+		{
+			identityArgs: identityArgs{
+				secretKeyHex: "0xfc83355e0327740b200467e63bbda96b551a617be7d16c91ced7107fa53f9304",
+				secretHex:    "0xd76d8f806a8afee1fe85a9971b98814f045ca89cf2d0c1f7801cfb8ebe8016",
+				nullifierHex: "0x670add141d23ecb75e0644ef1d621300c7fa2178b756719c4320f9d85d71d7",
+			},
+		},
+	}
+
+	for i := range testCases {
+		testCases[i].provider = provider
+		testCases[i].registerArgs = env.registerArgs
 	}
 
 	for i, tc := range testCases {
@@ -36,13 +102,40 @@ func TestCreateAndRegister(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			txHash, err := postCalldata(relayerURL, cd)
+			txHash, err := postCalldata(env.relayerURL, cd)
 			if err != nil {
 				t.Fatal(fmt.Errorf("failed to post calldata: %v", err))
 			}
 
 			t.Logf("txHash: %s\n=== END OF RUN %d ===", txHash, i)
 		})
+	}
+}
+
+type envArgs struct {
+	relayerURL string
+	registerArgs
+}
+
+func fromEnv() envArgs {
+	mustEnv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			panic(fmt.Sprintf("env var %s must be set", k))
+		}
+		return v
+	}
+
+	return envArgs{
+		relayerURL: mustEnv("RELAYER_URL"),
+		registerArgs: registerArgs{
+			rarimoCoreURL:        mustEnv("RARIMO_CORE_URL"),
+			issuerDid:            mustEnv("ISSUER_DID"),
+			votingAddress:        mustEnv("VOTING_ADDRESS"), // what is this?
+			schemaJsonLd:         []byte(mustEnv("SCHEMA_JSON_LD")),
+			issuingAuthorityCode: mustEnv("ISSUING_AUTHORITY_CODE"),
+			stateInfoJSON:        []byte(mustEnv("STATE_INFO_JSON")),
+		},
 	}
 }
 
