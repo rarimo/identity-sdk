3548a5a88767a78a9cc20626da21fc2ca0e53350
Maksym Hrynenko <mg06062003@gmail.com>
2024-05-17 18:11:55 +0300

    add: state provider interface implementation for GetGISTProof and Fetch; update: rebase branch to fix/blocknumber-statehash

--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/rarimo/go-schema-processor v1.0.2
 	github.com/rarimo/registration-relayer v0.1.2
 	github.com/sirupsen/logrus v1.9.3
-	golang.org/x/crypto v0.19.0
+	golang.org/x/crypto v0.21.0
 )
 
 require (
@@ -29,15 +29,16 @@ require (
 	github.com/ethereum/c-kzg-4844 v0.4.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/rarimo/passport-identity-provider v0.9.6 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/supranational/blst v0.3.11 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	gitlab.com/distributed_lab/logan v3.8.1+incompatible // indirect
-	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
+	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
 	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/tools v0.21.0 // indirect
--- state_provider.go
+++ state_provider.go
@@ -1,1 +1,102 @@
 package identity
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/rarimo/passport-identity-provider/resources"
+	"gitlab.com/distributed_lab/logan/v3"
+	"gitlab.com/distributed_lab/logan/v3/errors"
+)
+
+type stateProvider struct {
+	identityProviderURL url.URL
+}
+
+func NewStateProvider() StateProvider {
+	return &stateProvider{}
+}
+
+func (s stateProvider) GetGISTProof(userId string, blockNumber string) ([]byte, error) {
+	providerURL := s.identityProviderURL.JoinPath("/integrations/identity-provider-service/v1/gist-data")
+	// sometimes blockNumber is empty
+	if userId == "" {
+		return nil, fmt.Errorf("user identifier is empty")
+	}
+
+	query := providerURL.Query()
+	query.Set("user_did", userId)
+	query.Set("block_number", blockNumber)
+	providerURL.RawQuery = query.Encode()
+
+	resp, err := http.Get(providerURL.String())
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to fetch identity provider gist data", logan.F{
+			"url": providerURL.String(),
+		})
+	}
+
+	var gistData resources.GistDataResponse
+	if err = json.NewDecoder(resp.Body).Decode(&gistData); err != nil {
+		return nil, errors.Wrap(err, "failed to parse identity provider gist data")
+	}
+
+	rawProof, err := json.Marshal(gistData.Data.Attributes.GistProof)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to encode gist proof")
+	}
+
+	return rawProof, nil
+}
+
+func (s stateProvider) ProveAuthV2(inputs []byte) ([]byte, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s stateProvider) Fetch(url string, method string, body []byte, headerKey string, headerValue string) ([]byte, error) {
+	if len(body) == 0 || url == "" || method == "" || headerKey == "" || headerValue == "" {
+		return nil, errors.From(errors.New("Fetch: some input is empty"), logan.F{
+			"url": url, "method": method, "body": string(body), "headerKey": headerKey, "headerValue": headerValue,
+		})
+	}
+
+	request, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to build request")
+	}
+
+	request.Header.Set(headerKey, headerValue)
+
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to make request")
+	}
+
+	defer resp.Body.Close()
+
+	var rawBody []byte
+	if err = json.NewDecoder(resp.Body).Decode(&rawBody); err != nil {
+		return nil, errors.Wrap(err, "failed to parse body")
+	}
+
+	return rawBody, nil
+}
+
+func (s stateProvider) LocalPrinter(msg string) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s stateProvider) ProveCredentialAtomicQueryMTPV2OnChainVoting(inputs []byte) ([]byte, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s stateProvider) IsUserRegistered(contract string, documentNullifier []byte) (bool, error) {
+	//TODO implement me
+	panic("implement me")
+}
